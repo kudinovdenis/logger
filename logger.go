62c6268dc820fd108f084aa6efcd6a0515b61a3e
denis.kudinov@acronis.com <Denis.Kudinov@acronis.com>
2017-03-14 15:10:09 +0300

    + drain body

--- logger.go
+++ logger.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"fmt"
 	"io/ioutil"
+	"io"
 	"net/http"
 	"strconv"
 	"bytes"
@@ -31,20 +32,21 @@ func Logf(level int, format string, a ...interface{}) {
 
 // LogResponse ... log response
 func LogResponse(res *http.Response, logBody bool) {
-	bodyString := ""
-	bodyBytes, _ := ioutil.ReadAll(res.Body)
-	// Restore the io.ReadCloser to its original state
-	res.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
-	// Use the content
-	bodyString += string(bodyBytes)
-
 	headersString := ""
 	for k, v := range res.Header {
 		headersString = headersString + fmt.Sprintf("[key:%s value:%s] ", k, v)
 	}
 
 	var message string
-	if logBody && len(bodyString) > 0 {
+	save, reader, err := drainBody(res.Body)
+	bodyBytes, _ := ioutil.ReadAll(reader)
+	if logBody && len(bodyBytes) > 0 {
+		if err != nil {
+			Logf(LogLevelError, "Cant parse response %s.", err.Error())
+		}
+		bodyString := ""
+		res.Body = save
+		bodyString += string(bodyBytes)
 		message = fmt.Sprintf("Response for [%s]: Status: %s. Headers: %s. Body: %s", res.Request.URL.String(), strconv.Itoa(res.StatusCode), headersString, bodyString)
 	} else {
 		message = fmt.Sprintf("Response for [%s]: Status: %s. Headers: %s.", res.Request.URL.String(), strconv.Itoa(res.StatusCode), headersString)
@@ -64,16 +66,32 @@ func LogRequest(req *http.Request, logBody bool) {
 	}
 	message := fmt.Sprintf("Request started: %s [%s]: Headers: %s.", req.Method, req.URL.String(), headersString)
 	if logBody && req.Body != nil {
-		reader, err := req.GetBody()
+		save, reader, err := drainBody(req.Body)
 		if err != nil {
-			Logf(LogLevelError, "Cant parse response %s.", err.Error())
+			Logf(LogLevelError, "Cant parse request %s.", err.Error())
 		}
 		body, err := ioutil.ReadAll(reader)
 		if err != nil {
-			Logf(LogLevelError, "Cant parse response %s.", err.Error())
+			Logf(LogLevelError, "Cant parse requesr %s.", err.Error())
 		}
 		bodyString := string(body[:])
 		message += fmt.Sprintf(" Body: %s", bodyString)
+		req.Body = save
 	}
 	Log(LogLevelDefault, message)
+}
+
+func drainBody(b io.ReadCloser) (r1, r2 io.ReadCloser, err error) {
+	if b == http.NoBody {
+		// No copying needed. Preserve the magic sentinel meaning of NoBody.
+		return http.NoBody, http.NoBody, nil
+	}
+	var buf bytes.Buffer
+	if _, err = buf.ReadFrom(b); err != nil {
+		return nil, b, err
+	}
+	if err = b.Close(); err != nil {
+		return nil, b, err
+	}
+	return ioutil.NopCloser(&buf), ioutil.NopCloser(bytes.NewReader(buf.Bytes())), nil
 }
