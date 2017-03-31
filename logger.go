0cb62b3613ac5304c9b4bedf45280287cfb66f23
denis.kudinov@acronis.com <Denis.Kudinov@acronis.com>
2017-03-31 13:21:04 +0300

    * logger body improvements

--- logger.go
+++ logger.go
@@ -43,20 +43,22 @@ func logResponse(res *http.Response, logBody bool, from bool) {
 		headersString = headersString + fmt.Sprintf("[key:%s value:%s] ", k, v)
 	}
 
-	var message string
-	save, reader, err := drainBody(res.Body)
-	bodyBytes, _ := ioutil.ReadAll(reader)
-	res.Body = save
-	if logBody && len(bodyBytes) > 0 {
+	message := fmt.Sprintf("Response for %s [%s]: Status: %s. Headers: %s.", res.Request.Method, res.Request.URL.String(), strconv.Itoa(res.StatusCode), headersString)
+	if logBody && res.Body != nil {
+		save, reader, err := readersFromReader(res.Body)
 		if err != nil {
 			Logf(LogLevelError, "Cant parse response %s.", err.Error())
 		}
-		bodyString := ""
-		bodyString += string(bodyBytes)
-		message = fmt.Sprintf("Response for %s [%s]: Status: %s. Headers: %s. Body: %s", res.Request.Method, res.Request.URL.String(), strconv.Itoa(res.StatusCode), headersString, bodyString)
-	} else {
-		message = fmt.Sprintf("Response for %s [%s]: Status: %s. Headers: %s.", res.Request.Method, res.Request.URL.String(), strconv.Itoa(res.StatusCode), headersString)
+		body, err := ioutil.ReadAll(reader)
+		if err != nil {
+			Logf(LogLevelError, "Cant parse response %s.", err.Error())
+		}
+		bodyString := string(body[:])
+		message += fmt.Sprintf(" Body: %s", bodyString)
+		res.Body.Close()
+		res.Body = ioutil.NopCloser(save)
 	}
+
 	if res.StatusCode >= 200 && res.StatusCode < 300 {
 		if from {
 			Log(LogLevelFromService, message)
@@ -84,7 +86,7 @@ func logRequest(req *http.Request, logBody bool, from bool) {
 	}
 	message := fmt.Sprintf("Request started: %s [%s]: Headers: %s.", req.Method, req.URL.String(), headersString)
 	if logBody && req.Body != nil {
-		save, reader, err := drainBody(req.Body)
+		save, reader, err := readersFromReader(req.Body)
 		if err != nil {
 			Logf(LogLevelError, "Cant parse request %s.", err.Error())
 		}
@@ -94,7 +96,8 @@ func logRequest(req *http.Request, logBody bool, from bool) {
 		}
 		bodyString := string(body[:])
 		message += fmt.Sprintf(" Body: %s", bodyString)
-		req.Body = save
+		req.Body.Close()
+		req.Body = ioutil.NopCloser(save)
 	}
 	if from {
 		Log(LogLevelFromService, message)
@@ -111,17 +114,12 @@ func LogRequestFromService(req *http.Request, logBody bool) {
 	logRequest(req, logBody, true)
 }
 
-func drainBody(b io.ReadCloser) (r1, r2 io.ReadCloser, err error) {
-	if b == http.NoBody {
-		// No copying needed. Preserve the magic sentinel meaning of NoBody.
-		return http.NoBody, http.NoBody, nil
+
+func readersFromReader(reader io.ReadCloser) (io.Reader, io.Reader, error) {
+	b, err := ioutil.ReadAll(reader)
+	defer reader.Close()
+	if err != nil {
+		return nil, nil, err
 	}
-	var buf bytes.Buffer
-	if _, err = buf.ReadFrom(b); err != nil {
-		return nil, b, err
-	}
-	if err = b.Close(); err != nil {
-		return nil, b, err
-	}
-	return ioutil.NopCloser(&buf), ioutil.NopCloser(bytes.NewReader(buf.Bytes())), nil
+	return bytes.NewBuffer(b), bytes.NewBuffer(b), nil
 }
